a431e2d433d90992d3943fb9e4c1e73e7ff54d90
Vicky-vky <vignesh01engr@gmail.com>
2023-06-23 20:45:40 +0530

    Intership Project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import UserDashboard from './components/User';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  const [data, setData] = useState([ 
+ 
+]);
+
+const user_request = async()=>{
+  const url = 'https://reqres.in/api/users?page=2';
+  const response = await fetch(url);
+  const responseJson = await response.json()
+  console.log(responseJson);
+  setData(responseJson.data)
+}
+
+useEffect(()=>{
+  user_request();
+},[]);
+  return (
+  <div>
+    <UserDashboard datas = {data}/>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/User.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./User.css"
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+const UserDashboard = (props) => {
+    const [searchuser, setSearchuser] = useState("")
+
+    return (
+        <>
+            <div className="Container">
+                <div className="search_inputContainer">
+                    <input id="searchInput" type="text " placeholder="Search user Name.."
+                        onChange={(event) => {
+                            setSearchuser(event.target.value);
+                        }} />
+ 
+                
+
+            </div>
+
+            <div className="template-container">
+                {
+                    props.datas.filter((val) => {
+                        if (searchuser == "") { return val; }
+                        else if (val.first_name.toLowerCase().includes(searchuser.toLowerCase())) { return val; }
+                    })
+                        .map((val) => {
+                            return (
+                                <div className="template" key={val.id} >
+
+                                    <Col xs={6} md={4}>
+                                        <Image className="user_image" src={val.avatar} alt="user_image" roundedCircle />
+                                    </Col>
+                                    <h2 className="user_id">{val.id}</h2>
+                                    <h1 className="f_name">{val.first_name}</h1>
+                                </div>
+                            )
+                        })
+                }
+            </div>
+            </div>
+        </>
+    );
+
+}
+
+export default UserDashboard;
